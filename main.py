9928360b5de1286d76ab81d1484fe4cd6f0bdf7d
Zhongren Shao <shao.zhongren@gmail.com>
2017-05-13 18:20:29 -0400

    Handles names and standardizes all lines to uppercase

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import requests, csv, os
+import os
+import csv
+import requests
 
 BASE_URL = "http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php"
 """
-term: http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php?&term[]=4168&c=desc&r=gradedist%20target=
-dept: http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php?&dept=AAAD&c=desc&r=gradedist%20target=
-course-subj: http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php?&subject=AAAD-A&c=desc&r=gradedist%20target=
-class: http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php?&subject=AAAD-A&crse=100&c=desc&r=gradedist%20target=
-prof: http://gradedistribution.registrar.indiana.edu/exportToSpreadsheet.php?&instrname=ROSA,IRIS&c=desc&r=gradedist%20target=
+term: ?&term[]=4168&c=desc&r=gradedist%20target=
+dept: ?&dept=AAAD&c=desc&r=gradedist%20target=
+course-subj: ?&subject=AAAD-A&c=desc&r=gradedist%20target=
+class: ?&subject=AAAD-A&crse=100&c=desc&r=gradedist%20target=
+prof: ?&instrname=ROSA,IRIS&c=desc&r=gradedist%20target=
 """
 
-def getChronologicalName(term_name):
+def get_chronological_name(term_name):
     return ' '.join(sorted(term_name.split(' ')))
 
-def readTermKeysToDict(file):
+def read_term_keys_to_dict(file):
     sem_keys = {}
     with open(file, "r") as handle:
         sem_keys_reader = csv.reader(handle)
@@ -21,7 +23,7 @@ def readTermKeysToDict(file):
             sem_keys[k] = row[1]
     return sem_keys
 
-def downloadTermTable(directory, term, term_name):
+def download_term_table(directory, term, term_name):
     req_params = {
         "term[]": term,
         "dept": "",
@@ -33,40 +35,76 @@ def downloadTermTable(directory, term, term_name):
     }
     print("Downloading grade data for " + term_name)
     with open(os.path.join(directory, term_name + ".csv"), 'wb') as handle:
-        csv = requests.get(BASE_URL, params=req_params, stream=True)
-        if not csv.ok:
+        downloaded_csv = requests.get(BASE_URL, params=req_params, stream=True)
+        if not downloaded_csv.ok:
             print("Error writing " + os.path.join(directory, term_name + ".csv"))
 
-        for block in csv.iter_content(1024):
+        for block in downloaded_csv.iter_content(1024):
             handle.write(block)
 
-def downloadAllTermTables(directory=os.path.join("data", "orig")):
-    sem_keys = readTermKeysToDict("sem_keys.csv")
+def download_all_term_tables(directory=os.path.join("data", "orig")):
+    sem_keys = read_term_keys_to_dict("sem_keys.csv")
     for term_key, term_name in sem_keys.items():
-        downloadTermTable(directory, term_key, getChronologicalName(term_name))
+        download_term_table(directory, term_key, get_chronological_name(term_name))
 
-def readTable(directory, term_name):
+def read_table(directory, term_name):
     with open(os.path.join(directory, term_name + ".csv"), "r") as handle:
         table = csv.DictReader(handle)
         for row in table:
             print(row)
 
-def mergeTables(directory, out_file):
+def handle_name(name_col):
+    if name_col == 'INSTRUCTOR NAME':
+        return 'LAST NAME","FIRST NAME","MIDDLE NAME'
+    elif name_col == '"':
+        return '","","'
+    elif ',' in name_col[1:]:
+        names = []
+        parts = name_col.split(',')
+        names.append(parts[0])
+        i = parts[1].find(' ')
+        names.append(parts[1][:i])
+        names.append(parts[1][i + 1:])
+        return '","'.join(names)
+    else:
+        names = []
+        names.append(name_col[:-1])
+        names.append(name_col[-1:])
+        names.append('')
+        return '","'.join(names)
+
+def handle_line(line):
+    count_quotes = 0
+    i = 0
+    while count_quotes < 19:
+        if line[i] == '"':
+            count_quotes += 1
+        i += 1
+    new_i = i
+    while count_quotes < 20:
+        if line[new_i] == '"':
+            count_quotes += 1
+        new_i += 1
+    return line[:i].upper() + handle_name(line[i:new_i - 1].upper()) + line[new_i - 1:].upper()
+    # return '"' + handle_name(line[i:new_i - 1].upper()) + line[new_i - 1:].upper()
+
+def merge_tables(directory, out_file):
     with open(out_file, 'w') as handle:
         for (i, table) in enumerate(os.listdir(directory)):
             for (j, line) in enumerate(open(os.path.join(directory, table), 'r')):
                 if line == "\n" or j == 0 and i != 0:
                     continue
-                handle.write(line)
+                handle.write(handle_line(line))
     print("Done writing to " + out_file)
 
-def printHeaders(directory, out_file):
+def print_headers(directory, out_file):
     with open(out_file, 'w') as handle:
         for table in os.listdir(directory):
             with open(os.path.join(directory, table), 'r') as table_file:
                 handle.write(table + ',' + table_file.__next__())
-# downloadAllTermTables()
 
-# readTable(os.path.join("data", "orig"), "2016 Fall")
-mergeTables(os.path.join("data", "orig"), "merged.csv")
-# printHeaders(os.path.join("data", "orig"), "headers.csv")
+if __name__ == "__main__":
+    # download_all_term_tables()
+    # readTable(os.path.join("data", "orig"), "2016 Fall")
+    merge_tables(os.path.join("data", "orig"), "merged.csv")
+    # printHeaders(os.path.join("data", "orig"), "headers.csv")
